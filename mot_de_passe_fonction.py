41ce6aee8f9d4d4921b493749b2df33767ff6941
jspit74 <jpspitoni@gmail.com>
2020-03-27 18:23:59 +0000

    second jet

--- mot_de_passe_fonction.py
+++ mot_de_passe_fonction.py
@@ -9,20 +9,11 @@ certain jeux de caractères
 import random
 from enum import Enum
 
-"""
-class Strategie(Enum):
-    import string
-    ALPHA = list(string.ascii_letters)
-    DIGIT = list(string.digits)
-    SPECIAL = list("&#@{}[]()\$")
-"""  
 
 
-class CONSTANTE(Enum):
-    a=1
-    B=2
-    
-def ale_cherche(liste,n):
+
+
+def alea_cherche(liste,n):
     """
     Cette fonction doit chercher dans une liste un élément pris aléatoirement
     liste : est la liste des éléments dont on veut faire un tirage
