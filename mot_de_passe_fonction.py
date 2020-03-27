211754d26c9c2193db8dd3d3123e7462a81abb15
jspit74 <jpspitoni@gmail.com>
2020-03-27 18:51:24 +0000

    modif 3

--- mot_de_passe_fonction.py
+++ mot_de_passe_fonction.py
@@ -7,9 +7,12 @@ certain jeux de caractères
 [a-z][A-Z] et[0-9] et [&#@{[(\])]}]
 """
 import random
+import string
 from enum import Enum
 
-
+ALPHA=list(string.ascii_letters)
+DIGIT=list(string.digits)
+CARACTERE=list("&#@{[(\])]}]")
 
 
 
@@ -21,4 +24,34 @@ def alea_cherche(liste,n):
     
     """
     return random.choices(liste, k=n)
+
+def mdp_temp(N):
+    """
+
+    Parameters
+    ----------
+    N : TYPE interger
+        DESCRIPTION. Nombre de caractères pour le mot de passe
+
+    Returns un mot de passe avec le bonnombre de caractère sosu forme de string
+    -------
+    None.
+
+    """
+    l_tout_caracteres=ALPHA+DIGIT+CARACTERE
+    l=random.choices(l_tout_caracteres,k=N)   
+    # cetrte boucle va nous assurer que le mdp de commence
+    # ni par un chiffre ni par un caractère spécial
+    while True:
+        if l[0] not in DIGIT and l[0] not in CARACTERE:
+            break
+        else:
+            l=random.choices(l_tout_caracteres,k=N)   
+    # verifie qu'on ne peut pas avoir deux caractères de même type      
+    # on parcours la liste de 0 à N-2
+    cpt=0       
+    return l
+        
+        
+        
 
